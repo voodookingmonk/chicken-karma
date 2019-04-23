59a4d7857e1c0bf6e8f4dd822c59c85d3a437d15
Vahur Vogt <43129530+vvogt@users.noreply.github.com>
2019-04-23 14:38:22 +0300

    playerChar animatsioon

--- js/game.js
+++ js/game.js
@@ -28,7 +28,7 @@ let BootScene = new Phaser.Class({
         this.load.tilemapTiledJSON('map', 'assets/map/map.json');
 
         // our two characters
-        this.load.spritesheet('player', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
+        this.load.spritesheet('player', 'assets/player.png', { frameWidth: 23, frameHeight: 35 });
 		this.load.spritesheet('npc', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
     this.load.spritesheet('npc2', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
     this.load.spritesheet('npc3', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
@@ -76,7 +76,7 @@ let WorldScene = new Phaser.Class({
         //  animation with key 'left', we don't need left and right as we will use one and flip the sprite
         this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13]}),
+            frames: this.anims.generateFrameNumbers('player', { frames: [12, 13, 14, 15]}),
             frameRate: 10,
             repeat: -1
         });
@@ -84,20 +84,20 @@ let WorldScene = new Phaser.Class({
         // animation with key 'right'
         this.anims.create({
             key: 'right',
-            frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13] }),
+            frames: this.anims.generateFrameNumbers('player', { frames: [8, 9, 10, 11] }),
             frameRate: 10,
             repeat: -1
         });
 
         this.anims.create({
             key: 'up',
-            frames: this.anims.generateFrameNumbers('player', { frames: [2, 8, 2, 14]}),
+            frames: this.anims.generateFrameNumbers('player', { frames: [4, 5, 6, 7]}),
             frameRate: 10,
             repeat: -1
         });
         this.anims.create({
             key: 'down',
-            frames: this.anims.generateFrameNumbers('player', { frames: [ 0, 6, 0, 12 ] }),
+            frames: this.anims.generateFrameNumbers('player', { frames: [0, 1, 2, 3] }),
             frameRate: 10,
             repeat: -1
         });
@@ -327,7 +327,7 @@ let WorldScene = new Phaser.Class({
           if (this.cursors.left.isDown)
           {
               this.player.anims.play('left', true);
-              this.player.flipX = true;
+              this.player.flipX = false;
           }
           else if (this.cursors.right.isDown)
           {
