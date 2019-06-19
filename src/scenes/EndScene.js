bad2ce3e057a4f2bf4ea61ac688a2c40dd0e73ca
Merilin Võrk <merilinv@tlu.ee>
2019-06-19 16:32:25 +0300

    mõned nupud ja muudatused

--- src/scenes/EndScene.js
+++ src/scenes/EndScene.js
@@ -24,19 +24,20 @@ export class EndScene extends Phaser.Scene{
         this.scene.stop('WorldScene');   
         this.scene.stop('UIScene');
         this.scene.stop('HelpScene');
+        console.log(gameScene.karma);
 
         if(gameScene.karma >= 0 && gameScene.karma < 33){
             console.log("worst");
             this.ending = this.add.text(200, 70, 'worst', { fill: '#0f0', fontSize: 24 });
             let worst = this.add.image(10, 70, "worst").setOrigin(0);
         }
-        if(gameScene.karma >= 33 && gameScene.karma < 66){
+        if(gameScene.karma >= 33 && gameScene.karma < 100){
             console.log("medium");
             this.ending = this.add.text(200, 70, 'medium', { fill: '#0f0', fontSize: 24 });
             let medium = this.add.image(10, 70, "medium").setOrigin(0);
             
         }
-        if(gameScene.karma >= 66 && gameScene.karma <= 100){
+        if(gameScene.karma == 100){
             console.log("best");
             this.ending = this.add.text(200, 70, 'best', { fill: '#0f0', fontSize: 24 });
             let best = this.add.image(10, 70, "best").setOrigin(0);
